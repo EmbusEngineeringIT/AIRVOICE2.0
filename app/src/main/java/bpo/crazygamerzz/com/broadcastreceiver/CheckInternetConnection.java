763409215a2c25c53fc62636ba7e727817781d62
Cheemala <chaitanya.ch@embus.in>
2018-02-20 00:18:41 +0530

    new code2.0

---
+++ app/src/main/java/bpo/crazygamerzz/com/broadcastreceiver/CheckInternetConnection.java
@@ -0,0 +1,24 @@
+package bpo.crazygamerzz.com.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Kumar on 12/21/2017.
+ */
+
+public class CheckInternetConnection {
+    public static boolean isNetworkState(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED || connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            return true;
+        } else
+            return false;
+    }
+}
